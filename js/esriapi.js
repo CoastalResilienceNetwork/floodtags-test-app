d8682a385c663d956329af9b48e8b047ca7209c0
Matt Silveira <msilveira@tnc.org>
2018-03-08 14:24:48 -0700

    revert back

--- js/esriapi.js
+++ js/esriapi.js
@@ -9,7 +9,27 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, Query
         return declare(null, {
 			esriApiFunctions: function(t){
 				// define dynamic layer numbers
-				
+				t.selectedBasin = 0;
+				t.selectedProfile = 2;
+				t.layerDefinitions = [];
+// build the dropdown menu for the profiles section with the code below. ////////////////////////////////////////////////////////////////////
+				var queryTask = new QueryTask(t.url + "/3")
+				var query = new Query();
+				query.returnGeometry = false;
+				query.where = "OBJECTID > -1"
+				queryTask.execute(query, lang.hitch(t, function(results){
+					var profiles = [];
+					$.each(results.features,lang.hitch(this, function(i,v){
+						profiles.push(v.attributes.name)
+					}));
+					profiles.sort()
+					$('#' + t.id + 'ch-pro').empty();
+					$('#' + t.id + 'ch-pro').append("<option value=''></option>")
+					$.each(profiles, lang.hitch(this, function(i,v){
+						$('#' + t.id + 'ch-pro').append("<option value='" + v + "'>" + v + "</option>")
+					})); 
+					$('#' + t.id + 'ch-pro').trigger("chosen:updated");
+				}));
 					
 // Add dynamic map service ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.7});
@@ -24,52 +44,52 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, Query
 					t.map.setMapCursor("pointer");
 					
 // State set work on dynamic layer load ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-					// if (t.obj.stateSet == "yes"){
-					// 	if(t.obj.accordActive == 1){
-					// 		t.obj.selectedProWhere = '';
-					// 	} else if(t.obj.accordActive == 2){
-					// 		t.obj.selectedBasinWhere = ''
-					// 	}else{
-					// 		t.obj.selectedProWhere = '';
-					// 		t.obj.selectedBasinWhere = ''
-					// 	}
-					// 	// accordion visibility
-					// 	$('#' + t.id + t.obj.accordVisible).show();
-					// 	$('#' + t.id + t.obj.accordHidden).hide();
-					// 	$('#' + t.id + 'getHelpBtn').html(t.obj.buttonText);
-					// 	t.clicks.updateAccord(t);
-					// 	$('#' + t.id + t.obj.accordVisible).accordion( "option", "active", t.obj.accordActive );
+					if (t.obj.stateSet == "yes"){
+						if(t.obj.accordActive == 1){
+							t.obj.selectedProWhere = '';
+						} else if(t.obj.accordActive == 2){
+							t.obj.selectedBasinWhere = ''
+						}else{
+							t.obj.selectedProWhere = '';
+							t.obj.selectedBasinWhere = ''
+						}
+						// accordion visibility
+						$('#' + t.id + t.obj.accordVisible).show();
+						$('#' + t.id + t.obj.accordHidden).hide();
+						$('#' + t.id + 'getHelpBtn').html(t.obj.buttonText);
+						t.clicks.updateAccord(t);
+						$('#' + t.id + t.obj.accordVisible).accordion( "option", "active", t.obj.accordActive );
 
-					// 	// work with depletion section button
-					// 	if(t.obj.sliderPlayBtn == 'play'){
-					// 		$('#' + t.id + 'sliderPlay').trigger('click');
-					// 	}
-					// 	// Work with cat section in save and share
-					// 	t.clicks.categorySelComplete(t);
-					// 	// save and share category section
-					// 	if (t.obj.selectedBasinWhere.length > 0){
-					// 		var q = new Query();
-					// 		q.where = t.obj.selectedBasinWhere;
-					// 		t.category.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
-					// 	} 
-					// 	// save and share profile section
-					// 	if(t.obj.selectedProWhere.length > 0){
-					// 		var q = new Query();
-					// 		q.where = t.obj.selectedProWhere;
-					// 		t.profileDD.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
-					// 		$('#' + t.id + 'ch-pro').val(t.obj.profileName).trigger('chosen:updated').trigger('change', p);
-					// 	}
-					// 	t.layerDefinitions[0] = t.obj.selectedBasinWhere;
-					// 	t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
-					// 	// Update the visible layers from set state vis layers
-					// 	t.dynamicLayer.setVisibleLayers(t.obj.setStateVisLayers);
-					// 	t.obj.stateSet = "no";
-					// }else{
-					// 	// set the initial visible layers on app load
-					// 	t.obj.visibleLayers = [5];
-					// 	t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-					// 	//t.clicks.layerDefsUpdate(t);
-					// }
+						// work with depletion section button
+						if(t.obj.sliderPlayBtn == 'play'){
+							$('#' + t.id + 'sliderPlay').trigger('click');
+						}
+						// Work with cat section in save and share
+						t.clicks.categorySelComplete(t);
+						// save and share category section
+						if (t.obj.selectedBasinWhere.length > 0){
+							var q = new Query();
+							q.where = t.obj.selectedBasinWhere;
+							t.category.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
+						} 
+						// save and share profile section
+						if(t.obj.selectedProWhere.length > 0){
+							var q = new Query();
+							q.where = t.obj.selectedProWhere;
+							t.profileDD.selectFeatures(q,esri.layers.FeatureLayer.SELECTION_NEW);
+							$('#' + t.id + 'ch-pro').val(t.obj.profileName).trigger('chosen:updated').trigger('change', p);
+						}
+						t.layerDefinitions[0] = t.obj.selectedBasinWhere;
+						t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
+						// Update the visible layers from set state vis layers
+						t.dynamicLayer.setVisibleLayers(t.obj.setStateVisLayers);
+						t.obj.stateSet = "no";
+					}else{
+						// set the initial visible layers on app load
+						t.obj.visibleLayers = [5];
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+						//t.clicks.layerDefsUpdate(t);
+					}
 				}));
 // Work with feature layers and map clicks //////////////////////////////////////////////////////////////////////////////////////////////////////////				
 				// set category layer
--- main.js
+++ main.js
@@ -52,8 +52,24 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 		// Called when user hits 'Save and Share' button. This creates the url that builds the app at a given state using JSON. 
 		// Write anything to you varObject.json file you have tracked during user activity.		
 		getState: function () {
-			state = this.obj;
-			return state;	
+			// remove this conditional statement when minimize is added
+			if ( $('#' + this.id ).is(":visible") ){
+				this.obj.setStateVisLayers = this.obj.visibleLayers;
+				if ( $('#' + this.id + 'mainAccord').is(":visible") ){
+					this.obj.accordVisible = 'mainAccord';
+					this.obj.accordHidden = 'infoAccord';
+				}else{
+					this.obj.accordVisible = 'infoAccord';
+					this.obj.accordHidden = 'mainAccord';
+				}	
+				this.obj.accordActive = $('#' + this.id + this.obj.accordVisible).accordion( "option", "active" );
+				this.obj.buttonText = $('#' + this.id + 'getHelpBtn').html();
+				this.obj.extent = this.map.geographicExtent;
+				this.obj.stateSet = "yes";	
+				var state = new Object();
+				state = this.obj;
+				return state;	
+			}
 		},
 		// Called before activate only when plugin is started from a getState url. 
 		//It's overwrites the default JSON definfed in initialize with the saved stae JSON.
@@ -66,7 +82,41 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 		},	
 		// Called by activate and builds the plugins elements and functions
 		render: function() {
+			// BRING IN OTHER JS FILES
+			this.barChart = new barChart();
+			this.hbar = new hbar();
+			this.esriapi = new esriapi();
+			this.clicks = new clicks();
+			this.chartjs = new chartjs();
+			// ADD HTML TO APP
+			// Define Content Pane as HTML parent		
+			this.appDiv = new ContentPane({style:'padding:0; color:#000; flex:1; display:flex; flex-direction:column;}'});
+			this.id = this.appDiv.id
+			dom.byId(this.container).appendChild(this.appDiv.domNode);					
+			$('#' + this.id).parent().addClass('sty_flexColumn')
+			$('#' + this.id).addClass('accord')
+			if (this.obj.stateSet == "no"){
+				$('#' + this.id).parent().parent().css('display', 'flex')
+			}
+			// Get html from content.html, prepend appDiv.id to html element id's, and add to appDiv
+			var idUpdate0 = content.replace(/for="/g, 'for="' + this.id);	
+			var idUpdate = idUpdate0.replace(/id="/g, 'id="' + this.id);
+			$('#' + this.id).html(idUpdate);
 			
+			
+			// add this.yearDiv like above and add styling 
+			this.yearDiv = new ContentPane({style:'display:none; padding:0; color:#000; opacity: 0.9; margin-right:145px; flex:1; z-index:1000; position: absolute; top: 27px; left: 50%; text-align:center; border-radius:1px; -moz-box-shadow:0 1px 2px rgba(0,0,0,0.5); -webkit-box-shadow: 0 1px 2px rgba(0,0,0,0.5); box-shadow: 0 1px 2px rgba(0,0,0,0.5); }'});
+			this.yearID = this.yearDiv.id;
+			dom.byId('map-0').appendChild(this.yearDiv.domNode);
+			//
+			$('#' + this.yearID).html('<div class="sh_yearMapText" id="yearMapText"></div>');
+			// $('#map-0').append()
+			// CREATE ESRI OBJECTS AND EVENT LISTENERS	
+			this.esriapi.esriApiFunctions(this);
+			// Click listeners
+			this.clicks.clickListener(this);
+			// set up chartjs charts
+			this.chartjs.createChart(this);
 			this.rendered = true;	
 		}
 	});
